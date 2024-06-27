24addba13c8e33a7ebed153ed03f2c43d663eedf
martinvruiz <martinvruiz10@gmail.com>
2024-06-26 22:13:37 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById('menu-button')
+
+
+btn.addEventListener('click', function() {
+    var menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');})
+
